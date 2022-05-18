10777baf79d5effd7c3009580ff4b847ae8b85bc
kananimaulik26 <91308028+kananimaulik26@users.noreply.github.com>
2022-05-18 14:49:08 +0530

    added new changes

--- src/Components/AppointmentSlot.js
+++ src/Components/AppointmentSlot.js
@@ -5,7 +5,7 @@ import { Tab, Tabs } from "react-bootstrap";
 import "react-calendar/dist/Calendar.css";
 import "../Assets/Css/AppointmentSlot.css";
 
-const Hours = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
+const Hours = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
 const Minutes = [0, 15, 30, 45];
 
 const AppointmentSlot = () => {
